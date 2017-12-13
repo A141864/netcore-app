701b70a9c862517cd9c800adc3f7eac6318e185a
Flynn Handley <flynnhandley@Flynns-MacBook-Pro.local>
2017-12-14 09:59:06 +1100

    Initial Commit

---
+++ make.sh
@@ -0,0 +1,34 @@
+function build {
+    docker run -it -v $(pwd):/app microsoft/aspnetcore-build:2.0 \
+        dotnet build /app/aspnetapp.csproj
+}
+
+function unit {
+ docker run -it -v $(pwd):/app microsoft/aspnetcore-build:2.0 \
+    dotnet test /app/aspnetapp.csproj   
+}
+
+function bundle {
+ docker run -it -v $(pwd):/app microsoft/aspnetcore-build:2.0 \
+    dotnet pack --output /app/nupkgs /app/aspnetapp.csproj
+}
+
+function deploy {
+    echo "Deploy not yet implemented..."
+    exit 1
+}
+
+while test $# -gt 0
+do
+    case "$1" in
+        --build) build
+        ;;
+        --test) unit
+        ;;
+        --bundle) bundle
+        ;;
+        --deploy) deploy
+        ;;
+    esac
+    shift
+done
